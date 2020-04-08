ef0d621bf9f5f5686cf736a0166c7edb0663c72c
Maicon Douglas Filipiaki <mdfilipiaki@gmail.com>
2020-04-08 10:39:59 -0300

    :construction: Create Login

--- fiCos/blueprints/api/v1/login/Resources.py
+++ fiCos/blueprints/api/v1/login/Resources.py
@@ -1,11 +1,31 @@
-from flask import jsonify
+from flask import jsonify, request, current_app
 from flask_restful import Resource
 from flasgger import swag_from
 
+import datetime
+import jwt
+
+from fiCos.models.User import User
 class LoginResource(Resource):
 
     @swag_from('static/username_specs.yml', methods=['POST'])
     def post(self):
+        email = request.json.get('email')
+        password = request.json.get('password')
+
+        user = User.query.filter_by(email=email).first_or_404()
+
+        if not user.verify_password(password):
+            return jsonify({
+                "error":"credentials are not correct"
+            }), 403
+
+        payload = {
+            "id": user.id,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+        }
+        token = jwt.encode(payload, current_app.config.get('SECRET_KEY'))
+
         return jsonify(
-            {"ok":"ok"}
+            {"token": token.decode('utf-8') }
         )
